a62b2c5505cb28bb68f7969038fe11023eb5e38d
clebercmb <cleber@gmail.com>
2020-09-23 15:13:53 -0300

    Authentication Route

--- gobarber-app/src/Pages/SignIn/index.tsx
+++ gobarber-app/src/Pages/SignIn/index.tsx
@@ -1,31 +1,76 @@
 import React from 'react';
-import { Image } from 'react-native';
+import {
+  Image,
+  View,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 import logoImg from '../../assets/logo.png';
 
-import { Container, Title } from './styles';
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountButtonText,
+} from './styles';
 
 const SignIn: React.FC = () => {
   return (
-    <Container>
-      <Image source={logoImg} />
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1 }}
+        >
+          <Container>
+            <Image source={logoImg} />
 
-      <Title>Sign-In</Title>
+            <View>
+              <Title>Sign-In</Title>
+            </View>
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="password" icon="lock" placeholder="Password" />
 
-      <Input name="email" icon="mail" placeholder="E-mail" />
-      <Input name="password" icon="lock" placeholder="Password" />
+            <Button
+              onPress={() => {
+                console.log('>>>>Hi');
+              }}
+            >
+              Enter
+            </Button>
 
-      <Button
+            <ForgotPassword
+              onPress={() => {
+                console.log('Button Clicked');
+              }}
+            >
+              <ForgotPasswordText>Forgot my password</ForgotPasswordText>
+            </ForgotPassword>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      <CreateAccountButton
         onPress={() => {
-          console.log('>>>>Hi');
+          console.log('>> CreateAccountButton');
         }}
       >
-        Enter
-      </Button>
-    </Container>
+        <Icon name="log-in" size={20} color="#FF9000" />
+        <CreateAccountButtonText>Create an account</CreateAccountButtonText>
+      </CreateAccountButton>
+    </>
   );
 };
 
