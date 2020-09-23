0f0ff9c51ca3949979b28bc9cbda4f9406036f96
clebercmb <cleber@gmail.com>
2020-09-23 13:43:36 -0300

    Fixed islint import

---
+++ gobarber-app/src/Pages/SignIn/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Image } from 'react-native';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import logoImg from '../../assets/logo.png';
+
+import { Container, Title } from './styles';
+
+const SignIn: React.FC = () => {
+  return (
+    <Container>
+      <Image source={logoImg} />
+
+      <Title>Sign-In</Title>
+
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Password" />
+
+      <Button
+        onPress={() => {
+          console.log('>>>>Hi');
+        }}
+      >
+        Enter
+      </Button>
+    </Container>
+  );
+};
+
+export default SignIn;
---
+++ gobarber-app/src/components/Button/styles.ts
@@ -0,0 +1,25 @@
+import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
+
+// Always you are going to style a
+// component out side the React,
+// you must use () around the component
+//
+// For standard all the elements have
+// display: flex
+export const Container = styled(RectButton)`
+  width: 100%;
+  height: 60px;
+  background: #ff9000;
+  border-radius: 10px;
+  margin-top: 8px;
+
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ButtonText = styled.Text`
+  font-family: 'RobotoSlab-Medium';
+  color: #312e38;
+  font-size: 18px;
+`;
