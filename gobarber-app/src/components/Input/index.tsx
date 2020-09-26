851dfa2296c0c8e5ab4bba13eff504e0b68a610a
clebercmb <cleber@gmail.com>
2020-09-26 03:14:37 -0300

    improve input component

--- gobarber-app/src/components/Input/index.tsx
+++ gobarber-app/src/components/Input/index.tsx
@@ -1,4 +1,9 @@
-import React, { useEffect, useRef } from 'react';
+import React, {
+  useEffect,
+  useRef,
+  useImperativeHandle,
+  forwardRef,
+} from 'react';
 import { TextInputProps } from 'react-native';
 import { useField } from '@unform/core';
 
@@ -13,11 +18,24 @@ interface InputValueReference {
   value: string;
 }
 
-const Input: React.FC<InputProps> = ({ name, icon, ...rest }: InputProps) => {
+interface InputRef {
+  focus(): void;
+}
+
+const Input: React.ForwardRefRenderFunction<InputRef, InputProps> = (
+  { name, icon, ...rest }: InputProps,
+  ref,
+) => {
   const inputElementRef = useRef<any>(null);
 
   const { registerField, defaultValue = '', fieldName, error } = useField(name);
   const inputValueRef = useRef<InputValueReference>({ value: '' });
+
+  useImperativeHandle(ref, () => ({
+    focus() {
+      inputElementRef.current.focus();
+    },
+  }));
 
   useEffect(() => {
     registerField<string>({
@@ -51,4 +69,4 @@ const Input: React.FC<InputProps> = ({ name, icon, ...rest }: InputProps) => {
     </Container>
   );
 };
-export default Input;
+export default forwardRef(Input);
