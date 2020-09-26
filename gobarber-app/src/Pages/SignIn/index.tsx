2febc67cc7da65a36769779ba9963d70300aaf05
clebercmb <cleber@gmail.com>
2020-09-25 22:30:17 -0300

    Create an account link

--- gobarber-app/src/Pages/SignIn/index.tsx
+++ gobarber-app/src/Pages/SignIn/index.tsx
@@ -1,31 +1,54 @@
 import React from 'react';
 import { Image } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 import logoImg from '../../assets/logo.png';
 
-import { Container, Title } from './styles';
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountButtonText,
+} from './styles';
 
 const SignIn: React.FC = () => {
   return (
-    <Container>
-      <Image source={logoImg} />
+    <>
+      <Container>
+        <Image source={logoImg} />
 
-      <Title>Sign-In</Title>
+        <Title>Sign-In</Title>
 
-      <Input name="email" icon="mail" placeholder="E-mail" />
-      <Input name="password" icon="lock" placeholder="Password" />
+        <Input name="email" icon="mail" placeholder="E-mail" />
+        <Input name="password" icon="lock" placeholder="Password" />
 
-      <Button
-        onPress={() => {
-          console.log('>>>>Hi');
-        }}
-      >
-        Enter
-      </Button>
-    </Container>
+        <Button
+          onPress={() => {
+            console.log('>>>>Hi');
+          }}
+        >
+          Enter
+        </Button>
+
+        <ForgotPassword
+          onPress={() => {
+            console.log('Sign Button');
+          }}
+        >
+          <ForgotPasswordText>Forgot my password</ForgotPasswordText>
+        </ForgotPassword>
+      </Container>
+
+      <CreateAccountButton onPress={() => {}}>
+        <Icon name="log-in" size={20} color="#ff9000" />
+        <CreateAccountButtonText>Create an account</CreateAccountButtonText>
+      </CreateAccountButton>
+    </>
   );
 };
 
