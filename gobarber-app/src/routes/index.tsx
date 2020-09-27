8669d1a87a8c946a9ff5f82c495a9102d2296800
clebercmb <cleber@gmail.com>
2020-09-27 01:53:22 -0300

    Private Routes

--- gobarber-app/src/routes/index.tsx
+++ gobarber-app/src/routes/index.tsx
@@ -1,22 +1,22 @@
 import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
+import { View, ActivityIndicator } from 'react-native';
 
-import SignIn from '../Pages/SignIn';
-import SignUp from '../Pages/SignUp';
+import { useAuth } from '../hooks/auth';
+import AppRoutes from './app.routes';
+import AuthRoutes from './auth.routes';
 
-const Auth = createStackNavigator();
+const Routes: React.FC = () => {
+  const { user, loading } = useAuth();
 
-const AuthRoutes: React.FC = () => (
-  <Auth.Navigator
-    screenOptions={{
-      headerShown: false,
-      cardStyle: { backgroundColor: '#312e38' },
-    }}
-    // initialRouteName="SignUp"
-  >
-    <Auth.Screen name="SignIn" component={SignIn} />
-    <Auth.Screen name="SignUp" component={SignUp} />
-  </Auth.Navigator>
-);
+  if (loading) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size="large" color="#999" />
+      </View>
+    );
+  }
+  // console.log('****user=', user);
+  return user ? <AppRoutes /> : <AuthRoutes />;
+};
 
-export default AuthRoutes;
+export default Routes;
