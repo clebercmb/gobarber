dca831c37d820acb556ffce6f25039770a06340a
clebercmb <cleber@gmail.com>
2020-09-29 19:52:35 -0300

    Thinking on tests

--- gobarber-back/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ gobarber-back/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,11 @@
-test('sum two number', () => {
-  expect(1 + 2).toBe(3);
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', () => {
+    expect(1 + 2).toBe(3);
+  });
+
+  it('shoud not be able to create two appointments at the same time', () => {
+    expect(1 + 2).toBe(3);
+  });
 });
