1d172d6c4374fd1db4f8b38e4cb3c629aa333834
clebercmb <cleber@gmail.com>
2020-10-07 02:49:51 -0300

    List of available days

--- gobarber-back/src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
+++ gobarber-back/src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -1,6 +1,7 @@
 // Service will always have a unique method inside of it
 // It will never have more than one method
 
+import { getDaysInMonth, getDate } from 'date-fns';
 import { inject, injectable } from 'tsyringe';
 
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
@@ -35,8 +36,45 @@ class ListProviderMonthAvailabilityService {
         month,
       },
     );
-    // console.log('appointments====>', appointments);
-    return [{ day: 1, available: false }];
+
+    // const numberOfDaysInMonth = getDaysInMonth(new Date(year, month - 1));
+
+    // const eachDayArray = Array.from(
+    //   { length: numberOfDaysInMonth },
+    //   (value, index) => index + 1,
+    // );
+
+    const eachDayArray = this.createArrayOfDaysInMonth(year, month);
+
+    const availability = eachDayArray.map(day => {
+      const appointmentsInDay = appointments.filter(appointment => {
+        return getDate(appointment.date) === day;
+      });
+
+      return {
+        day,
+        available: appointmentsInDay.length < 10,
+      };
+      //  08:30 09:30 10:30 11:30 12:30 13:30 14:30 15:30 16:30
+      // 8 9 10 11 12 13 14 15 16 17
+    });
+
+    return availability;
+  }
+
+  private createArrayOfDaysInMonth(year: number, month: number): number[] {
+    const numberOfDaysInMonth = this.getNumberOfDaysInMonth(year, month);
+
+    const daysInMonthOfYear = Array.from(
+      { length: numberOfDaysInMonth },
+      (value, index) => index + 1,
+    );
+
+    return daysInMonthOfYear;
+  }
+
+  private getNumberOfDaysInMonth(year: number, month: number): number {
+    return getDaysInMonth(new Date(year, month - 1));
   }
 }
 
