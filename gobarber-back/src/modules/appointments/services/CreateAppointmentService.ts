2725210cccf7ade30eae7396f155eeeca6cf1354
clebercmb <cleber@gmail.com>
2020-10-12 19:48:18 -0300

    Appointment rules

--- gobarber-back/src/modules/appointments/services/CreateAppointmentService.ts
+++ gobarber-back/src/modules/appointments/services/CreateAppointmentService.ts
@@ -4,7 +4,7 @@
 // It must NOT have any other functionality
 
 // This service will be responsible only for the schedule of an appointment
-import { startOfHour } from 'date-fns';
+import { startOfHour, isBefore, getHours } from 'date-fns';
 import { injectable, inject } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
@@ -43,12 +43,27 @@ class CreateAppointmentService {
     user_id,
   }: IRequest): Promise<Appointment> {
     const appointmentDate = startOfHour(date);
+
     const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(
       appointmentDate,
     );
 
+    if (this.checkIfAppointmentHourIsBeforeCurrentHour(appointmentDate)) {
+      throw new AppError('You cannot create an appointment on a pst date');
+    }
+
     if (findAppointmentInSameDate) {
       throw new AppError('This appointment is already booked');
+    }
+
+    if (user_id === provider_id) {
+      throw new AppError('You cannot create an appointment for yourself');
+    }
+
+    if (getHours(appointmentDate) < 8 || getHours(appointmentDate) > 17) {
+      throw new AppError(
+        'You can only make appointments between 08am and 05pm',
+      );
     }
 
     const appointment = await this.appointmentsRepository.create({
@@ -58,6 +73,12 @@ class CreateAppointmentService {
     });
 
     return appointment;
+  }
+
+  private checkIfAppointmentHourIsBeforeCurrentHour(
+    appointmentDate: Date,
+  ): boolean {
+    return isBefore(appointmentDate, Date.now());
   }
 }
 
