db859a640c1d3e6371f3e038d141639aebee5dbd
clebercmb <cleber@gmail.com>
2020-10-20 01:17:29 -0300

    Calendar Styles

--- gobarber-web/src/pages/Dashboard/index.tsx
+++ gobarber-web/src/pages/Dashboard/index.tsx
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
+import DayPicker, { DayModifiers } from 'react-day-picker';
+import 'react-day-picker/lib/style.css';
 import {
   Container,
   Header,
@@ -18,6 +20,12 @@ import { useAuth } from '../../hooks/auth';
 
 const Dashboard: React.FC = () => {
   const [selectedDate, setSelectedDate] = useState(new Date());
+
+  const handleDateChange = useCallback((day: Date, modifiers: DayModifiers) => {
+    if (modifiers.available) {
+      setSelectedDate(day);
+    }
+  }, []);
 
   const { signOut, user } = useAuth();
 
@@ -113,7 +121,18 @@ const Dashboard: React.FC = () => {
             </Section>
           </Section>
         </Schedule>
-        <Calendar></Calendar>
+        <Calendar>
+          <DayPicker
+            weekdaysShort={['S', 'M', 'T', 'W', 'T', 'F', 'S']}
+            fromMonth={new Date()}
+            disabledDays={[{ daysOfWeek: [0, 6] }]}
+            selectedDays={selectedDate}
+            onDayClick={handleDateChange}
+            modifiers={{
+              available: { daysOfWeek: [1, 2, 3, 4, 5] },
+            }}
+          />
+        </Calendar>
       </Content>
     </Container>
   );
