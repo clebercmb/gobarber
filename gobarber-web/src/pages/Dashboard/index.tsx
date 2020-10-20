d05184002882c897f32082fe5c5b5ea29dd43c7b
clebercmb <cleber@gmail.com>
2020-10-20 00:22:30 -0300

    Apponintments list

--- gobarber-web/src/pages/Dashboard/index.tsx
+++ gobarber-web/src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Container,
   Header,
@@ -6,6 +6,8 @@ import {
   Profile,
   Content,
   NextAppointment,
+  Section,
+  Appointment,
   Schedule,
   Calendar,
 } from './styles';
@@ -15,6 +17,8 @@ import { FiClock, FiPower } from 'react-icons/fi';
 import { useAuth } from '../../hooks/auth';
 
 const Dashboard: React.FC = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
   const { signOut, user } = useAuth();
 
   return (
@@ -57,6 +61,57 @@ const Dashboard: React.FC = () => {
               </span>
             </div>
           </NextAppointment>
+
+          <Section>
+            <strong>Morning</strong>
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars1.githubusercontent.com/u/18425787?s=460&u=692e0e711e5fe60188a1518cc9a2f7bae3bd6624&v=4"
+                  alt="Cleber Miranda"
+                />
+                <strong>Cleber Miranda</strong>
+              </div>
+            </Appointment>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars1.githubusercontent.com/u/18425787?s=460&u=692e0e711e5fe60188a1518cc9a2f7bae3bd6624&v=4"
+                  alt="Cleber Miranda"
+                />
+                <strong>Cleber Miranda</strong>
+              </div>
+            </Appointment>
+
+            <Section>
+              <strong>Afternoon</strong>
+              <Appointment>
+                <span>
+                  <FiClock />
+                  08:00
+                </span>
+
+                <div>
+                  <img
+                    src="https://avatars1.githubusercontent.com/u/18425787?s=460&u=692e0e711e5fe60188a1518cc9a2f7bae3bd6624&v=4"
+                    alt="Cleber Miranda"
+                  />
+                  <strong>Cleber Miranda</strong>
+                </div>
+              </Appointment>
+            </Section>
+          </Section>
         </Schedule>
         <Calendar></Calendar>
       </Content>
