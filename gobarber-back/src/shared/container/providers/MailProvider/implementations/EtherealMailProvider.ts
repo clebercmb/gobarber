de801c78dd441ef869c6395d743b1b288843dc4b
clebercmb <cleber@gmail.com>
2020-10-20 18:48:18 -0300

    Showing appointmentscd

--- gobarber-back/src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
+++ gobarber-back/src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
@@ -57,6 +57,6 @@ export default class EtherealMailProvider implements IMailProvider {
 
     console.log('Message sent: %s', message.messageId);
     // // Preview only available when sending through an Ethereal account
-    // console.log('Preview URL: %s', nodemailer.getTestMessageUrl(message));
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(message));
   }
 }
