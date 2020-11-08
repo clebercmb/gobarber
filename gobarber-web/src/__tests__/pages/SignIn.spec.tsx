fb97dcd99ba4e15e09f7e82d7acd70812e31ff75
clebercmb <cleber@gmail.com>
2020-11-08 18:48:39 -0300

    Authentication hook mock

--- gobarber-web/src/__tests__/pages/SignIn.spec.tsx
+++ gobarber-web/src/__tests__/pages/SignIn.spec.tsx
@@ -1,22 +1,45 @@
 import React from 'react';
 
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, screen, waitFor } from '@testing-library/react';
 
 import SignIn from '../../pages/SignIn';
 
+const mockedHistoryPush = jest.fn();
+
 jest.mock('react-router-dom', () => {
   return {
-    useHistory: jest.fn(),
+    useHistory: () => ({
+      push: mockedHistoryPush,
+    }),
     // In react "children" represents the content inside a component
     Link: ({ children }: { children: React.ReactNode }) => children,
   };
 });
 
+jest.mock('../../hooks/auth', () => {
+  return {
+    useAuth: () => ({
+      signIn: jest.fn(),
+    }),
+  };
+});
+
 describe('SignIn Page', () => {
-  it('should be able to sign in', () => {
+  it('should be able to sign in', async () => {
     const { getByPlaceholderText } = render(<SignIn />);
 
     const emailField = getByPlaceholderText('E-mail');
     const passwordField = getByPlaceholderText('Password');
+
+    const buttonElement = screen.getByText('Enter');
+
+    fireEvent.change(emailField, { target: { value: 'johndoe@example.com' } });
+    fireEvent.change(passwordField, { target: { value: '123456' } });
+
+    fireEvent.click(buttonElement);
+
+    await waitFor(() => {
+      expect(mockedHistoryPush).toHaveBeenCalledWith('/dashboard');
+    });
   });
 });
